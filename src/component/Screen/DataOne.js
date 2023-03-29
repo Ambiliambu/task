1431e7f26f0350f51b95835315c2075832c385bc
ambili <ambilipakhd@gmail.com>
2023-03-29 14:23:57 +0530

    second  commit

--- src/component/Screen/DataOne.js
+++ src/component/Screen/DataOne.js
@@ -1,6 +1,5 @@
 export const DataOne=[
-    // getData() {
-        // return[
+  
             {
                id:1,
                name: 'James Butt',
@@ -11,38 +10,75 @@ export const DataOne=[
             },
             {
                 id:2,
+                name: 'Ammi',
+                subject:'Physics',
+                mark:'30',
+                age:'18',
+                status:'failed'
+            },
+            {
+                id:3,
                 name: 'James Butt',
+                subject:'CS',
+                mark:'100',
+                age:'18',
+                status:'passed'
+            },
+            {
+                id:4,
+                name: 'Kalyani',
                 subject:'Physics',
                 mark:'50',
                 age:'18',
                 status:'passed'
-            },
-        //     {
-        //         id:3,
-        //         name: 'James Butt',
-        //         subject:'Physics',
-        //         mark:'50',
-        //         age:'18',
-        //         status:'passed'
-        //     },
-        //     {
-        //         id:4,
-        //         name: 'James Butt',
-        //         subject:'Physics',
-        //         mark:'50',
-        //         age:'18',
-        //         status:'passed'
-        // },
-        // {
-        //     id:5,
-        //     name: 'James Butt',
-        //     subject:'Physics',
-        //     mark:'50',
-        //     age:'18',
-        //     status:'passed'
-        // }
+        },
+        {
+            id:5,
+            name: 'Kevin',
+            subject:'Malayalam',
+            mark:'50',
+            age:'18',
+            status:'passed'
+        },
+        {
+            id:6,
+            name: 'Butt',
+            subject:'Physics',
+            mark:'40',
+            age:'18',
+            status:'failed'
+         },
+         {
+             id:7,
+             name: 'Mathew',
+             subject:'Hindi',
+             mark:'50',
+             age:'18',
+             status:'passed'
+         },
+         {
+             id:8,
+             name: 'Binu',
+             subject:'English',
+             mark:'70',
+             age:'18',
+             status:'passed'
+         },
+         {
+             id:9,
+             name: 'Anil',
+             subject:'Chemistry',
+             mark:'30',
+             age:'18',
+             status:'failed'
+     },
+     {
+         id:10,
+         name: 'Kiran',
+         subject:'Maths',
+         mark:'40',
+         age:'18',
+         status:'failed'
+     }
            
-           
-        // ];
-    // },
-]
+    ]
--- src/component/Screen/DataTwo.js
+++ src/component/Screen/DataTwo.js
@@ -1,6 +1,5 @@
 export const DataTwo=[
-    // getData() {
-        // return[
+  
             {
                id:1,
                name: 'James Butt',
@@ -9,36 +8,65 @@ export const DataTwo=[
             },
             {
                 id:2,
-                name: 'James Butt',
+                name: 'Jenny',
+                age:'17',
+                place:'Karnadaka'
+            },
+            {
+                id:3,
+                name: 'Ali',
+                age:'18',
+                place:'Kannur'
+            },
+            {
+                id:4,
+                name: 'Banu',
                 age:'18',
                 place:'Kerala'
-            },
-        //     {
-        //         id:3,
-        //         name: 'James Butt',
-        //    
-        //   
-        //         age:'18',
-        //         place:'Kerala'
-        //     },
-        //     {
-        //         id:4,
-        //         name: 'James Butt',
-        //    
-        //   
-        //         age:'18',
-        //         place:'Kerala'
-        // },
-        // {
-        //     id:5,
-        //     name: 'James Butt',
-        //
-        //
-        //     age:'18',
-        //     place:'Kerala'
-        // }
+        },
+        {
+            id:5,
+            name: 'Bavya',
+            age:'17',
+            place:'Kozhikode'
+        },
+        {
+            id:6,
+            name: 'Banu',
+            age:'17',
+            place:'Wayand'
+        },
+        {
+            id:7,
+            name: 'vanitha',
+            age:'18',
+            place:'Kasaragod'
+        },
+        {
+            id:8,
+            name: 'Kiran',
+            age:'17',
+            place:'Kozhikode'
+        },
+        {
+            id:9,
+            name: 'Kali',
+            age:'17',
+            place:'Kozhikode'
+        },
+        {
+            id:10,
+            name: 'Bavya',
+            age:'17',
+            place:'Kozhikode'
+        },{
+            id:11,
+            name: 'Lilli',
+            age:'17',
+            place:'Kerala'
+        }
            
            
-        // ];
-    // },
+        
+
 ]
